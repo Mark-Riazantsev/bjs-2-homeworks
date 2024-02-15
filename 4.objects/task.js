27ddc7f534c1b4f5f785836e3fdeb62640183dc6
Riazantsev <riazantsevmark327@gmail.com>
2024-02-15 22:12:42 +0400

    homework4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
-}
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.setSubject =function(subjectName) {
+    this.subject = subjectName;
+  };
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
 
-Student.prototype.getAverage = function () {
-  
-}
+this.addMarks = function(...marksToAdd) {
+    if (!this.marks) {
+        console.log("Error: Student has been expelled.");
+        return;
+    }
+    this.marks.push(...marksToAdd);
+};
 
-Student.prototype.exclude = function (reason) {
-  
+this.getAverage = function() {
+    if (!this.marks || !this.marks.length) {
+        return 0;
+    }
+    const sum = this.marks.reduce((acc, curr) => acc + curr);
+    return sum / this.marks.length;
+};
+
+this.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.exclude = reason;
+};
 }
